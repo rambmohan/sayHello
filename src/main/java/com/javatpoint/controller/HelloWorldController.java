9f8eb71fb12d7359c2083ddb6dcbbaace5468abe
Gitea <gitea@fake.local>
2023-04-02 19:42:09 +0530

    added new api 1

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.javatpoint.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
@@ -13,7 +14,7 @@ return "Hello ***************";
 
 
 @RequestMapping("/{name}")
-    public String hello(String name)
+    public String hello(@RequestParam String name)
     {
         return "Hello *************** "+name;
     }
